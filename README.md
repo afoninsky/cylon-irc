6205ff3a727841760b477749c9ea533949aedcb5
Andrey <vkfont@gmail.com>
2015-11-06 23:22:46 +0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cylon-natural
 Add ability to understand natural human language and communicate with friends
+
+Part of internal project, description not specified.
--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 'use strict';
 
-var Adaptor = require('./lib/adaptor'),
-    Driver = require('./lib/driver');
+var Driver = require('./lib/driver');
 
 module.exports = {
-  adaptors: ['natural'],
   drivers: ['natural'],
-  adaptor: function(opts) {
-    return new Adaptor(opts);
-  },
   driver: function(opts) {
     return new Driver(opts);
   }
